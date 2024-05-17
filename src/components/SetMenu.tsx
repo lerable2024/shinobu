8202f1580dba86630f7e33cbf1761e27170dc5cb
shiba <shibatakitakuya@shibanoMacBook-Pro.local>
2024-05-17 16:09:11 +0900

    add setMenu

---
+++ src/components/SetMenu.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from '@mui/material'
+import Subtitle from './SubTitle'
+
+export default function SetMenu() {
+	return (
+		<>
+			<Subtitle />
+			<Box sx={{ color: '#5F6527', px: 2 }}>
+				<Typography>骨盤・卵巣・子宮メンテナンス</Typography>
+			</Box>
+		</>
+	)
+}
